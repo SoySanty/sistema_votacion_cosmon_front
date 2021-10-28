4740f350b9766c78a21540406758bd35777a8dd0
Santiago Arias <sanc7us@gmail.com>
2021-10-28 14:47:28 -0400

    Last changes before first version

--- src/components/context/GlobalContext.js
+++ src/components/context/GlobalContext.js
@@ -1,92 +1,93 @@
-import React, { createContext, useEffect, useState } from 'react'
-import { MainUrl } from '../scripts/MainUrl';
+import React, { createContext, useEffect, useState } from "react";
+import { MainUrl } from "../scripts/MainUrl";
 
 //Obtener lista de eventos y ejecutar un callback
-const getEventList = async props => {
-   const {executeFn} = props;
-   const request = await fetch(`${MainUrl}?show=evento&target=mostrar_eventos`) 
-   const response = await request.json();
-   executeFn(response);
-}
+const getEventList = async (props) => {
+  const { executeFn } = props;
+  const request = await fetch(`${MainUrl}?show=evento&target=mostrar_eventos`);
+  const response = await request.json();
+  executeFn(response);
+};
 
 //Obtener lista de candidatos y ejecutar un callback
 const getCandidateList = async (props) => {
-   const {executeFn, target} = props;
-   const body = new FormData();
-   body.append('nro_categoria', target);
-   const url = `${MainUrl}?show=candidato&target=filtro1_candidatos`;
-   const data = { method: 'POST', body: body }
-   const request = await fetch(url, data)
-   const response = await request.json()
-   executeFn(response)
-}
-
+  const { executeFn, target } = props;
+  const body = new FormData();
+  body.append("nro_categoria", target);
+  const url = `${MainUrl}?show=candidato&target=filtro1_candidatos`;
+  const data = { method: "POST", body: body };
+  const request = await fetch(url, data);
+  const response = await request.json();
+  executeFn(response);
+};
 
 export const DataExport = createContext();
 export const GlobalContext = ({ children }) => {
-   
-   //=================Candidate Screen
-   const [imgCache, setImgCache] = useState(Math.random())
-   
-   //=================Home Screen
-   const [eventList, setEventList] = useState([]);
-   
-   useEffect(()=>{ //Llamar Lista de Eventos
-      getEventList({executeFn: setEventList})
-   }, [setEventList]);
+  //=================Candidate Screen
+  const [imgCache, setImgCache] = useState(Math.random());
 
-   const refreshEventList = () => getEventList({executeFn: setEventList}); //Actualizar la lista de eventos
-   const refreshCanddiateList = () => getCandidateList({
-      executeFn: setCandidateList, 
-      target: categoryTarget
-   }); //Actualizar la lista de eventos
+  //=================Home Screen
+  const [eventList, setEventList] = useState([]);
 
-   const [candidateList, setCandidateList] = useState() 
-   const [categoryTarget, setCategoryTarget] = useState(0)
+  useEffect(() => {
+    //Llamar Lista de Eventos
+    getEventList({ executeFn: setEventList });
+  }, [setEventList]);
 
-   useEffect(()=>{ //Llamar Lista de Candidatos
-         getCandidateList({
-            executeFn: setCandidateList, 
-            target: categoryTarget
-         });
-   }, [categoryTarget]);
+  const refreshEventList = () => getEventList({ executeFn: setEventList }); //Actualizar la lista de eventos
+  const refreshCanddiateList = () =>
+    getCandidateList({
+      executeFn: setCandidateList,
+      target: categoryTarget,
+    }); //Actualizar la lista de eventos
 
-   //=================Event Screen
-   const [eventTarget, setEventTarget] = useState(0)
-   const [dateLimit, setDateLimit] = useState(false)
-   const getDate = () => { //Obtener fecha
-      let date = new Date().toLocaleString("en-US", {timeZone: "America/New_York"})
-      date = new Date(date)
-      return date
-   }
-   const toDate = e => { //De string a date
-      let date = new Date(e)
-      return date
-   }
-   
+  const [candidateList, setCandidateList] = useState();
+  const [categoryTarget, setCategoryTarget] = useState(0);
 
-   //=================Objeto con las variables a exportar
-   const dataExport = {
-      eventList,
-      categoryTarget,
-      setCategoryTarget,
-      candidateList,
-      setCandidateList,
-      eventTarget,
-      setEventTarget,
-      refreshEventList,
-      imgCache,
-      setImgCache,
-      refreshCanddiateList,
-      getDate,
-      toDate,
-      dateLimit,
-      setDateLimit
-   }
-   return (
-      <DataExport.Provider value={dataExport} >
-         {children}
-      </DataExport.Provider>
-   );
+  useEffect(() => {
+    //Llamar Lista de Candidatos
+    getCandidateList({
+      executeFn: setCandidateList,
+      target: categoryTarget,
+    });
+  }, [categoryTarget]);
 
-}
+  //=================Event Screen
+  const [eventTarget, setEventTarget] = useState(0);
+  const [dateLimit, setDateLimit] = useState(false);
+  const getDate = () => {
+    //Obtener fecha
+    let date = new Date().toLocaleString("en-US", {
+      timeZone: "America/New_York",
+    });
+    date = new Date(date);
+    return date;
+  };
+  const toDate = (e) => {
+    //De string a date
+    let date = new Date(e);
+    return date;
+  };
+
+  //=================Objeto con las variables a exportar
+  const dataExport = {
+    eventList,
+    categoryTarget,
+    setCategoryTarget,
+    candidateList,
+    setCandidateList,
+    eventTarget,
+    setEventTarget,
+    refreshEventList,
+    imgCache,
+    setImgCache,
+    refreshCanddiateList,
+    getDate,
+    toDate,
+    dateLimit,
+    setDateLimit,
+  };
+  return (
+    <DataExport.Provider value={dataExport}>{children}</DataExport.Provider>
+  );
+};
--- src/components/scripts/AsignCandidates.js
+++ src/components/scripts/AsignCandidates.js
@@ -1,4 +1,4 @@
-import { MainUrl} from "./MainUrl"
+import { MainUrl } from "./MainUrl";
 
 /*
 En este archivo se encuantran las funciones que generan
@@ -7,126 +7,124 @@ candidatos
 */
 
 //Para refrescar la lista de candidatos
-let refresh = () => {}
+let refresh = () => {};
 
-export const asignCandidateForm = props => {
+export const asignCandidateForm = (props) => {
+  const { nro, nombre, eventList, setModalData, refreshEventList } = props;
 
-   const {
-      nro,
-      nombre,
-      eventList,
-      setModalData,
-      refreshEventList
-   } = props
+  const content = (
+    <>
+      <label htmlFor="fecha_final">Evento</label>
+      <select
+        onChange={(e) => {
+          candidateList(e.target.value);
+        }}
+      >
+        <option value={0} hidden>
+          Seleccione un evento
+        </option>
+        {eventList ? (
+          eventList.map((e, index) => (
+            <option value={e.nro} key={index}>
+              {e.evento}
+            </option>
+          ))
+        ) : (
+          <></>
+        )}
+      </select>
+      <label htmlFor="fecha_final">Categoría</label>
+      <select name="categoria" id="formSelectCatList">
+        <option value={0} hidden>
+          Seleccione una categoría
+        </option>
+      </select>
+      <input type="hidden" name="candidato" defaultValue={nro} />
+    </>
+  );
 
-   const content = (
-      <> 
-         <label htmlFor="fecha_final">Evento</label>
-         <select
-         onChange={ e=>{candidateList(e.target.value)} }
-         >
-         <option value={0} hidden>Seleccione un evento</option>
-            {
-            eventList?
-            eventList.map((e, index) => (
-               <option value={e.nro} key={index}>{e.evento}</option>
-            ))
-            : <></>
-            }
-         </select>
-         <label htmlFor="fecha_final">Categoría</label>
-         <select name="categoria" id="formSelectCatList">
-            <option value={0} hidden>Seleccione una categoría</option>
-         </select>
-         <input type="hidden" name="candidato" defaultValue={nro} />
-      </>
-   )
+  refresh = refreshEventList;
 
-   refresh = refreshEventList
+  setModalData({
+    type: "form",
+    title: nombre,
+    formId: "asignarCandidato",
+    content,
+    onSubmit: asignCandidate,
+  });
+};
 
-   setModalData({
-      type: "form",
-      title: nombre,
-      formId: "asignarCandidato",
-      content,
-      onSubmit: asignCandidate
-   })
-   
-}
+export const removeCandidateForm = (props) => {
+  const { nro, category, nombre, setModalData, refreshCanddiateList } = props;
 
-export const removeCandidateForm = props => {
-   const {
-      nro,
-      category,
-      nombre,
-      setModalData,
-      refreshCanddiateList
-   } = props
+  const content = (
+    <>
+      <p>
+        ¿Desea desvincular <b>{nombre}</b>de esta lista?
+      </p>
+      <input type="hidden" name="candidato" defaultValue={nro} />
+      <input type="hidden" name="categoria" defaultValue={category} />
+    </>
+  );
 
-   const content = 
-   <>
-   <p>¿Desea desvincular <b>{nombre}</b>de esta lista?</p>
-   <input type="hidden" name="candidato" defaultValue={nro} />
-   <input type="hidden" name="categoria" defaultValue={category} />
-   </>
+  refresh = refreshCanddiateList;
 
-   refresh = refreshCanddiateList
-
-   setModalData({
-      type: "form",
-      title: "Desvincular",
-      formId: "asignarCandidato",
-      content,
-      onSubmit: removeCandidateList
-   })
-}
+  setModalData({
+    type: "form",
+    title: "Desvincular",
+    formId: "asignarCandidato",
+    content,
+    onSubmit: removeCandidateList,
+  });
+};
 
 export const asignCandidate = async (form) => {
-   const formData = new FormData(form)
-   const url = `${MainUrl}?show=candidato_categoria&target=asignar_candidato`
-   const data = {
-      method: 'POST',
-      body: formData
-   }
-   const request = await fetch(url, data)
-   const response = await request.text()
-   refresh()
-   return response
-}
+  const formData = new FormData(form);
+  const url = `${MainUrl}?show=candidato_categoria&target=asignar_candidato`;
+  const data = {
+    method: "POST",
+    body: formData,
+  };
+  const request = await fetch(url, data);
+  const response = await request.text();
+  refresh();
+  return response;
+};
 
 //Refrescar el select de la ventana modal
 const candidateList = async (target) => {
-   const body = new FormData()
-      body.append("nro_evento", target)
-      const url = `${MainUrl}?show=categoria&target=filtrar_categoria`
-      const data = {
-         method: "POST",
-         body
-      }
-      const request = await fetch(url, data)
-      const response = await request.json()
+  const body = new FormData();
+  body.append("nro_evento", target);
+  const url = `${MainUrl}?show=categoria&target=filtrar_categoria`;
+  const data = {
+    method: "POST",
+    body,
+  };
+  const request = await fetch(url, data);
+  const response = await request.json();
 
-      if(response){
-         const formSelectCatList = document.querySelector("#formSelectCatList")
-         formSelectCatList.innerHTML = "<option value='0' hidden>Seleccione una categoría</option>";
-         response.forEach(e =>{
-            formSelectCatList.innerHTML += `
+  if (response) {
+    const formSelectCatList = document.querySelector("#formSelectCatList");
+    formSelectCatList.innerHTML =
+      "<option value='0' hidden>Seleccione una categoría</option>";
+    response.forEach((e) => {
+      formSelectCatList.innerHTML += `
             <option value="${e.nro}" >${e.categoria}</option>
             `;
-         })
-      }
-}
+    });
+  }
+};
 
 //Eliminar el select de la ventana modal
 const removeCandidateList = async (form) => {
-   const body = new FormData(form)
-      const url = `${MainUrl}?show=candidato_categoria&target=desvincular_candidato`
-      const data = {
-         method: "POST",
-         body
-      }
-      const request = await fetch(url, data)
-      const response = await request.json()
-      refresh()
-      return response
-}
+  const body = new FormData(form);
+  const url = `${MainUrl}?show=candidato_categoria&target=desvincular_candidato`;
+  const data = {
+    method: "POST",
+    body,
+  };
+  const request = await fetch(url, data);
+  const response = await request.json();
+  refresh();
+  return response;
+};
